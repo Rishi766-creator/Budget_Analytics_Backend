0f019ba101c8989d550f93b0afedfb9d47f423ac
Rishi766-creator <kshatri.rishithabai@gmail.com>
2026-02-28 15:21:45 +0530

    url updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app=express();
 app.use(
   cors({
-    origin:[ "http://localhost:5173","https://budget-analytics-frontend-gx29.vercel.app/login"],
+    origin:[ "http://localhost:5173","https://budget-analytics-frontend.vercel.app/login"],
     credentials: true,
   })
 );
